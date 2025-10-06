a627dee4926fd965c7c19f5e1f0ee9af68a1ada5
Viktor Kuharenko <houkiro86@gmail.com>
2025-10-06 12:46:17 +0300

    переделал dateTime на stopWatch, добавил расчет времени выполнения, добавил переводы строк для удобного чтения

--- Program.cs
+++ Program.cs
@@ -1,19 +1,32 @@
-﻿Console.WriteLine($"Начало синхронных задач {DateTime.Now}");
+﻿using System.Diagnostics;
+
+var stopwatch = new Stopwatch();
+
+Console.WriteLine("Начало синхронных задач\n");
+stopwatch.Start();
 Console.WriteLine(ProcessData("Файл 1"));
 Console.WriteLine(ProcessData("Файл 2"));
 Console.WriteLine(ProcessData("Файл 3"));
-Console.WriteLine($"Завершение синхронных задач {DateTime.Now}\n");
+stopwatch.Stop();
+var syncTime = stopwatch.Elapsed.TotalSeconds;
+Console.WriteLine($"\nЗавершение синхронных задач. Время: {syncTime:F3} сек\n");
 
-Console.WriteLine($"Начало асинхронных задач {DateTime.Now}");
+Console.WriteLine("Начало асинхронных задач\n");
+stopwatch.Restart();
 string[] results = await Task.WhenAll(
     ProcessDataAsync("Асинхронно файл 1"),
     ProcessDataAsync("Асинхронно файл 2"),
     ProcessDataAsync("Асинхронно файл 3")
 );
-Console.WriteLine($"Завершение асинхронных задач {DateTime.Now}\n");
+
+stopwatch.Stop();
+var asyncTime = stopwatch.Elapsed.TotalSeconds;
+Console.WriteLine($"Завершение асинхронных задач. Время: {asyncTime:F3} сек\n");
 
 foreach (string result in results)
     Console.WriteLine(result);
+
+Console.WriteLine($"\nАсинхронно выполнено быстрее на {(syncTime - asyncTime):F3} сек");
 
 static string ProcessData(string dataName)
 {
